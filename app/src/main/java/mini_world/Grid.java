ad92a11652a64d2ea642e6b76a3bfd4c37a64f4e
VillageR88 <villager88pl@gmail.com>
2025-08-13 15:39:23 +0200

    Initial empty world simulation
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

---
+++ app/src/main/java/mini_world/Grid.java
@@ -0,0 +1,14 @@
+package mini_world;
+
+public class Grid {
+
+  private char[][] grid = new char[10][10];
+
+  public char[][] getGrid() {
+    return grid;
+  }
+
+  public void setGrid(char[][] grid) {
+    this.grid = grid;
+  }
+}
