c95a9d21fd44d7a3fe68de448141a3a1f04112e2
VillageR88 <villager88pl@gmail.com>
2025-08-14 01:52:19 +0200

    Initial Unit generation and Added Unit class
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/src/main/java/mini_world/Grid.java
+++ app/src/main/java/mini_world/Grid.java
@@ -10,7 +10,7 @@ public class Grid {
   }
 
   public void setGrid(int x, int y, Entity entity) {
-    this.grid[x][y] = entity;
+    this.grid[y][x] = entity;
   }
 
   public int getTime() {
@@ -21,9 +21,49 @@ public class Grid {
     return time % 7 == 1;
   }
 
+  @SuppressWarnings("UnnecessaryContinue")
   public void produceUnits() {
-    for (Entity[] row : grid) {
-      for (Entity element : row) {}
+    for (int y = 0; y < 10; y++) {
+      for (int x = 0; x < 10; x++) {
+        Entity entity = grid[y][x];
+        if (entity != null && entity.canSpawnEntity) {
+          int firstSectorX = x - 1;
+          int firstSectorY = y - 1;
+          int[][] possibleDeploymentLocations = new int[9][2];
+          int possibleDeploymentLength = 0;
+          for (
+            int vertical = firstSectorY;
+            vertical < firstSectorY + 3;
+            vertical++
+          ) for (
+            int horizontal = firstSectorX;
+            horizontal < firstSectorX + 3;
+            horizontal++
+          ) {
+            if (horizontal < 0 || horizontal > 9) continue; // X is out of boundaries
+            else if (vertical < 0 || vertical > 9) continue; // Y is out of boundaries
+            else if (grid[vertical][horizontal] != null) continue; // Sector is already taken by -> grid[vertical][horizontal].symbol
+            else {
+              possibleDeploymentLocations[possibleDeploymentLength][0] =
+                vertical;
+              possibleDeploymentLocations[possibleDeploymentLength][1] =
+                horizontal;
+              possibleDeploymentLength++;
+            }
+          }
+          if (possibleDeploymentLength > 0) {
+            int rolledDeployment = (int) (
+              Math.random() * possibleDeploymentLength
+            );
+            int dY = possibleDeploymentLocations[rolledDeployment][0];
+            int dX = possibleDeploymentLocations[rolledDeployment][1];
+            char unitSymbol = Character.toLowerCase(entity.symbol);
+            int unitSide = entity.side;
+            Unit unit = new Unit(unitSymbol, unitSide);
+            grid[dY][dX] = unit;
+          }
+        }
+      }
     }
   }
 
---
+++ app/src/main/java/mini_world/Unit.java
@@ -0,0 +1,8 @@
+package mini_world;
+
+public class Unit extends Entity {
+
+  public Unit(char symbol, int side) {
+    super(false, true, true, symbol, side);
+  }
+}
